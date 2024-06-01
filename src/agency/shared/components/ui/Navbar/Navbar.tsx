79da585c99a3ce09214fefb7e03973c590c6543c
Adrian <alextravc.342@gmail.com>
2024-06-01 12:42:24 -0500

    ADD: Nabvar

--- src/agency/shared/components/ui/Navbar/Navbar.tsx
+++ src/agency/shared/components/ui/Navbar/Navbar.tsx
@@ -1,7 +1,66 @@
+import { useState } from 'react';
+import { FaBars, FaTimes } from 'react-icons/fa';
+import { IoLogoBuffer } from 'react-icons/io5';
+import { Link } from 'react-scroll';
+
+import { tabs } from '@/agency/shared/constants';
+
+import './Navbar.css';
+
 export interface NavbarInterface {}
 
 const Navbar: React.FC<NavbarInterface> = () => {
-  return <nav id="navbar">Navbar</nav>;
+  const [viewSidebar, setViewSidebar] = useState<boolean>(false);
+
+  return (
+    <nav id="navbar">
+      <div className="logo__container">
+        <IoLogoBuffer className="icon" />
+
+        <h1 className="name">
+          Adin<span className="color__primary gradient__text">uba</span>
+        </h1>
+      </div>
+
+      <div className={`hamburger ${viewSidebar ? 'visible' : ''}`}>
+        <div className="tab__group">
+          <span
+            className="icon__container close__btn"
+            onClick={() => setViewSidebar(!viewSidebar)}
+          >
+            <FaTimes />
+          </span>
+
+          {tabs.map((tab, idx) => (
+            <Link
+              className="tab__item"
+              key={idx}
+              to={tab.id}
+              smooth={true}
+              spy={true}
+              offset={-70}
+              duration={500}
+              activeClass="active"
+              onClick={() => setViewSidebar(false)}
+            >
+              {tab.name}
+            </Link>
+          ))}
+        </div>
+      </div>
+
+      <div className="buttons__container">
+        <Link
+          to="contact"
+          smooth={true}
+          className="contact__btn btn btn__primary"
+        >
+          Cotáctanos
+        </Link>
+        <FaBars className="menu" onClick={() => setViewSidebar(!viewSidebar)} />
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
---
+++ src/agency/shared/constants/source.constants.ts
@@ -0,0 +1,11 @@
+export type TabContent = {
+  name: string;
+  id: string;
+};
+export const tabs: TabContent[] = [
+  { name: 'Home', id: 'hero' },
+  { name: 'Servicios', id: 'services' },
+  { name: 'Nosotros', id: 'about' },
+  { name: 'Proyectos', id: 'projects' },
+  { name: 'Equipo', id: 'teams' },
+];
